a995b91b14053449cc514735798444aec14e1e4b
pidud <TsuchitoriJoel@gmail.com>
2023-12-27 00:39:34 -1000

    started work on raymarching shader

--- src/lib.rs
+++ src/lib.rs
@@ -26,20 +26,72 @@ impl Vertex {
     }
 }
 
+struct Camera {
+    eye: cgmath::Point3<f32>,
+    target: cgmath::Point3<f32>,
+    up: cgmath::Vector3<f32>,
+    aspect: f32,
+    fovy: f32,
+    znear: f32,
+    zfar: f32,
+}
+
+#[rustfmt::skip]
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 0.5, 0.5,
+    0.0, 0.0, 0.0, 1.0,
+);
+
+
+impl Camera {
+    fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
+        let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
+        let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
+        return OPENGL_TO_WGPU_MATRIX * proj * view;
+    }
+}
+
+// We need this for Rust to store our data correctly for the shaders
+#[repr(C)]
+// This is so we can store this in a buffer
+#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct CameraUniform {
+    // We can't use cgmath with bytemuck directly, so we'll have
+    // to convert the Matrix4 into a 4x4 f32 array
+    view_proj: [[f32; 4]; 4],
+    camera_pos: [f32; 4],
+}
+
+impl CameraUniform {
+    fn new() -> Self {
+        use cgmath::SquareMatrix;
+        Self {
+            view_proj: cgmath::Matrix4::identity().into(),
+            camera_pos: [0.0, 0.0, 0.0, 0.0],
+        }
+    }
+
+    fn update_view_proj(&mut self, camera: &Camera) {
+        self.view_proj = camera.build_view_projection_matrix().into();
+        self.camera_pos = camera.eye.to_homogeneous().into();
+    }
+}
+
+
 // Vertices arranged ccw around normal face
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [-0.0868241, 0.49240386, 0.0], tex_coords: [0.4131759, 0.00759614], }, // A
-    Vertex { position: [-0.49513406, 0.06958647, 0.0], tex_coords: [0.0048659444, 0.43041354], }, // B
-    Vertex { position: [-0.21918549, -0.44939706, 0.0], tex_coords: [0.28081453, 0.949397], }, // C
-    Vertex { position: [0.35966998, -0.3473291, 0.0], tex_coords: [0.85967, 0.84732914], }, // D
-    Vertex { position: [0.44147372, 0.2347359, 0.0], tex_coords: [0.9414737, 0.2652641], }, // E
+    Vertex { position: [-1.0, -1.0, 0.0], tex_coords: [-1.0, -1.0], }, // A
+    Vertex { position: [-1.0, 1.0, 0.0], tex_coords: [-1.0, 1.0], }, // B
+    Vertex { position: [1.0, -1.0, 0.0], tex_coords: [1.0, -1.0], }, // C
+    Vertex { position: [1.0, 1.0, 0.0], tex_coords: [1.0, 1.0], }, // D
 ];
 
 //Store indices as well to avoid storing duplicate vertices
 const INDICES: &[u16] = &[
-    0, 1, 4,
-    1, 2, 4,
-    2, 3, 4,
+    0, 1, 2,
+    1, 2, 3,
 ];
 
 
@@ -57,6 +109,9 @@ pub struct WgpuState {
     pub num_indices: u32,
     pub diffuse_bind_group: wgpu::BindGroup,
     pub diffuse_texture: texture::Texture,
+    pub camera: Camera,
+    pub camera_buffer: wgpu::Buffer,
+    pub camera_bind_group: wgpu::BindGroup,
 }
 
 impl WgpuState {
@@ -89,6 +144,7 @@ impl WgpuState {
                 });
             rpass.set_pipeline(&self.pipeline);
             rpass.set_bind_group(0, &self.diffuse_bind_group, &[]);
+            rpass.set_bind_group(1, &self.camera_bind_group, &[]);
             rpass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
             rpass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
             rpass.draw_indexed(0..self.num_indices, 0, 0..1);
@@ -224,11 +280,64 @@ impl WgpuState {
                 label: Some("diffuse_bind_group"),
             }
         );
+
+        let camera = Camera {
+            // position the camera 1 unit up and 2 units back
+            // +z is out of the screen
+            eye: (0.0, 1.0, 2.0).into(),
+            // have it look at the origin
+            target: (0.0, 0.0, 0.0).into(),
+            // which way is "up"
+            up: cgmath::Vector3::unit_y(),
+            aspect: config.width as f32 / config.height as f32,
+            fovy: 45.0,
+            znear: 0.1,
+            zfar: 100.0,
+        };
+
+        let mut camera_uniform = CameraUniform::new();
+        let camera_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Camera Buffer"),
+                contents: bytemuck::cast_slice(&[camera_uniform]),
+                usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            }
+        );
+
+        let camera_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::VERTEX,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }
+            ],
+            label: Some("camera_bind_group_layout"),
+        });
+
+        let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &camera_bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: camera_buffer.as_entire_binding(),
+                }
+            ],
+            label: Some("camera_bind_group"),
+        });        
         
         let pipeline_layout = device.create_pipeline_layout(
             &wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[&texture_bind_group_layout], 
+                bind_group_layouts: &[
+                    &texture_bind_group_layout,
+                    &camera_bind_group_layout,
+                ], 
                 push_constant_ranges: &[],
             }
         );
@@ -267,7 +376,10 @@ impl WgpuState {
             num_vertices: VERTICES.len() as u32,
             num_indices: INDICES.len() as u32,
             diffuse_bind_group: diffuse_bind_group,
-            diffuse_texture: diffuse_texture
+            diffuse_texture: diffuse_texture,
+            camera: camera,
+            camera_buffer: camera_buffer,
+            camera_bind_group: camera_bind_group,
         }
     }
 }
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,4 +1,10 @@
 // Vertex shader
+struct CameraUniform {
+    view_proj: mat4x4<f32>,
+    camera_pos: vec4<f32>,
+};
+@group(1) @binding(0) // 1.
+var<uniform> camera: CameraUniform;
 
 struct VertexInput {
     @location(0) position: vec3<f32>,
@@ -8,6 +14,7 @@ struct VertexInput {
 struct VertexOutput {
     @builtin(position) clip_position: vec4<f32>,
     @location(0) tex_coords: vec2<f32>,
+    @location(1) view_dir: vec3<f32>,
 }
 
 @vertex
@@ -16,7 +23,8 @@ fn vs_main(
 ) -> VertexOutput {
     var out: VertexOutput;
     out.tex_coords = model.tex_coords;
-    out.clip_position = vec4<f32>(model.position, 1.0);
+    out.clip_position = camera.view_proj * vec4<f32>(model.position, 1.0);
+    out.view_dir = normalize(camera.camera_pos.xyz - model.position);
     return out;
 }
 
@@ -27,9 +35,21 @@ var t_diffuse: texture_2d<f32>;
 @group(0) @binding(1)
 var s_diffuse: sampler;
 
+fn sdf(in: vec3<f32>) -> f32 {
+    var center: vec3<f32> = vec3<f32> (0.0, -1.0, -1.0);
+    return distance(in, center) - 0.1;
+}
+
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return textureSample(t_diffuse, s_diffuse, in.tex_coords);
+    var position = in.clip_position.xyz;
+    for(var i = 0; i < 200; i++){
+        if(sdf(position) <= 0.1){
+            return vec4<f32>(0.0, 1.0, 0.0, 1.0);
+        }
+        position += in.view_dir * sdf(position);
+    }
+    return vec4<f32>(0.0, 0.0, 0.0, 1.0);
 }
 
  
